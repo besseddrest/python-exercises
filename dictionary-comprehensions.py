dbf737fa6356b148d5ef20f0ef9c536d8687353c
Harold <harold@Harolds-Hippo-MacBook-Pro.local>
2019-12-20 18:09:42 -0800

    add dictionary comprehensions file

---
+++ dictionary-comprehensions.py
@@ -0,0 +1,4 @@
+# General dictionary comprehension template:
+# dict_variable = {key:value for (key,value) in dictonary.items()}
+
+# Use a dictionary comprehension to count the length of each word in a sentence.
--- list-comprehensions.py
+++ list-comprehensions.py
@@ -26,8 +26,6 @@ remove_all_vowels = ''.join([i for i in lorem if not i in vowels])
 less_than_4 = [word for word in lorem.split(' ') if len(word) < 4]
 # print(less_than_4)
 
-# Use a dictionary comprehension to count the length of each word in a sentence.
-
 # Use a nested list comprehension to find all of the numbers from 1-1000 that are divisible by any single digit besides 1 (2-9)
 
 # For all the numbers 1-1000, use a nested list/dictionary comprehension to find the highest single digit any of the numbers is divisible by
