c78e2046e4cdffe70296f05988e0a61c24244f90
Harold <harold@Harolds-Hippo-MacBook-Pro.local>
2020-01-07 02:02:49 -0800

    add comments for valid parens

--- leetcode/20-valid-parentheses.py
+++ leetcode/20-valid-parentheses.py
@@ -11,34 +11,30 @@ Note that an empty string is also considered valid.
 """
 
 def isValid(self, s: str) -> bool:
+    # if blank
     if len(s) == 0:
-        return True
+            return True
 
+    # if odd number of chars or starts with closing bracket
     if len(s) % 2 == 1 or s[0] in ')}]':
         return False
         
     all = []
-    
+    # create a dict so we know the correct partner chars
     partners = {")": "(", "}": "{", "]": "["}
     
     for char in s:
+        # keep track of opened brackets in order
         if char in '([{':
             all.append(char)
             continue
 
+        # if we passed the above check, its a closing bracket
+        # pop the last character and see if it is the partner of this closing bracket
         if all.pop() != partners[char]:
             return False
-            
-    return len(all) == 0
-    
-    for char in s:
-        if char in '([{':
-            all.append(char)
-            continue
 
-        if char in ')}]' and all.pop() != partners[char]:
-            return False
-            
+    # we've gone through the string, all open brackets should have been popped    
     return len(all) == 0
 
 # Runtime: 
