bac21c4f063ba3de709a81eca83a320b3891c90b
Harold <harold@Harolds-Hippo-MacBook-Pro.local>
2020-01-07 14:28:35 -0800

    update remove element and comment

--- leetcode/27-remove-element.py
+++ leetcode/27-remove-element.py
@@ -10,26 +10,25 @@ Do not allocate extra space for another array, you must do this by modifying the
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 """
 
-# iterate over nums
-# if current value is equal to val
-# iterate over rest of list until we find the next diff val
-# take that diff val and replace current val
-# increment
-# return new length, or current index + 1
-
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # keeps track of non-matched values
         count = 0
+        # store original length
         length = len(nums)
 
+        # we use a range because we want to iterate over the original length
         for i in range(0, length):
+            # if we find a non-match
             if nums[i] != val:
+                # add it to the end of our list
                 nums.append(nums[i])
+                # increment count
                 count += 1
                 
+        # reverse the list so that all the unique values will be at the front of the list
         nums.reverse()
 
-        print(nums)
         return count
 
 # Runtime: 
